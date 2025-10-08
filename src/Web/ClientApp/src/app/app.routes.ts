2a58cea0b736119ff77237ebd65191484adc56b9
CoreDX1 <chrismaquis@gmail.com>
2025-10-08 11:50:29 -0300

    Add routing configuration for login and file explorer pages

--- src/Web/ClientApp/src/app/app.routes.ts
+++ src/Web/ClientApp/src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { FileExplorerPageComponent } from './file-explorer-page/file-explorer-page.component';
+import { AuthGuard } from './auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'explorer', 
+    component: FileExplorerPageComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: '', 
+    redirectTo: '/login', 
+    pathMatch: 'full'
+  }
+];
