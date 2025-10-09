6574d9f53c90049078fc29f3c22a93e6a65e729b
CoreDX1 <chrismaquis@gmail.com>
2025-10-09 10:48:32 -0300

    Add Entity Framework Core DbContext and initial migration for User entity

---
+++ src/Infrastructure/Data/FileExplorerDbContext.cs
@@ -0,0 +1,31 @@
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class FileExplorerDbContext : DbContext
+{
+    public FileExplorerDbContext(DbContextOptions<FileExplorerDbContext> options)
+        : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id);
+            entity.Property(e => e.Username).IsRequired().HasMaxLength(50);
+            entity.Property(e => e.Email).IsRequired().HasMaxLength(100);
+            entity.Property(e => e.PasswordHash).IsRequired();
+            entity.Property(e => e.FullName).HasMaxLength(100);
+            entity.Property(e => e.StorageQuotaBytes).IsRequired();
+            entity.Property(e => e.CreatedAt).IsRequired();
+            entity.Property(e => e.UpdatedAt).IsRequired();
+            entity.Property(e => e.IsActive).IsRequired();
+            entity.Property(e => e.LastLoginAt);
+
+            entity.HasIndex(e => e.Email).IsUnique();
+        });
+    }
+}
