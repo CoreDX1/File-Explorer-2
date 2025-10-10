9947e592c760c4deb5e4a293e25581942480731e
CoreDX1 <chrismaquis@gmail.com>
2025-10-10 14:12:10 -0300

    Add IRepositoryAsync interface for asynchronous data access operations

---
+++ src/Infrastructure/Interface/IRepositoryAsync.cs
@@ -0,0 +1,18 @@
+using TrackableEntities.Common.Core;
+
+namespace Infrastructure.Interface;
+
+public interface IRepositoryAsync<TEntity> : IRepository<TEntity>
+    where TEntity : class, ITrackable
+{
+    Task<TEntity> FindAsync(params object[] keyValues);
+    Task<TEntity> FindAsync(CancellationToken cancellationToken, params object[] keyValues);
+    Task<bool> DeleteAsync(params object[] keyValues);
+    Task<bool> DeleteAsync(CancellationToken cancellationToken, params object[] keyValues);
+    Task<IEnumerable<TEntity>> SelectQueryAsync(string query, params object[] parameters);
+    Task<IEnumerable<TEntity>> SelectQueryAsync(
+        string query,
+        CancellationToken cancellationToken,
+        params object[] parameters
+    );
+}
