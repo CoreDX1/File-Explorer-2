aba5a41c585b01daa87e96d1d6cdd884e13fee2d
CoreDX1 <chrismaquis@gmail.com>
2025-10-13 10:26:57 -0300

    Refactorizando UserSerivices para incluir IMapper dependecy y implementar el metodo CreateUSer

--- src/Application/Services/UserServices.cs
+++ src/Application/Services/UserServices.cs
@@ -1,4 +1,7 @@
+using Application.DTOs.Request;
+using Application.DTOs.Response;
 using Application.Interface;
+using AutoMapper;
 using Domain.Entities;
 using Infrastructure.Interface;
 using Microsoft.EntityFrameworkCore;
@@ -8,11 +11,13 @@ namespace Application.Services;
 public class UserServices : Service<User>, IUserServices
 {
     private readonly IRepositoryAsync<User> _repository;
+    private readonly IMapper _mapper;
 
-    public UserServices(IRepositoryAsync<User> repository)
+    public UserServices(IRepositoryAsync<User> repository, IMapper mapper)
         : base(repository)
     {
         _repository = repository;
+        _mapper = mapper;
     }
 
     public async Task<List<User>> GetAllUsers()
@@ -23,5 +28,21 @@ public class UserServices : Service<User>, IUserServices
     public async Task<User> GetUserByEmail(string email)
     {
         return await Queryable().FirstOrDefaultAsync(u => u.Email == email);
+    }
+
+    public async Task<ResponseDTO> CreateUser(CreateUserRequest request)
+    {
+        var user = _mapper.Map<User>(request);
+
+        Insert(user);
+
+        var response = new ResponseDTO()
+        {
+            Data = user,
+            Message = "User created successfully",
+            Success = true,
+        };
+
+        return response;
     }
 }
