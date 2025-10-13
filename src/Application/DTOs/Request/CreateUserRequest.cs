4e09dc84431446f2f378b6308cd823a9f21b4abb
CoreDX1 <chrismaquis@gmail.com>
2025-10-13 10:25:29 -0300

    Fix property name from 'Username' to 'UserName' and update FullName property implementation

--- src/Application/DTOs/Request/CreateUserRequest.cs
+++ src/Application/DTOs/Request/CreateUserRequest.cs
@@ -2,7 +2,9 @@ namespace Application.DTOs.Request;
 
 public class CreateUserRequest
 {
-    public string Username { get; set; } = string.Empty;
+    public string UserName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
-    public string FullName { get; set; } = string.Empty;
+    public string FullName => $"{UserName} {LastName}";
 }
