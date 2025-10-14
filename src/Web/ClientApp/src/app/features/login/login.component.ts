6d61604deec37a385235aa6d5bb4387873ca736d
CoreDX1 <chrismaquis@gmail.com>
2025-10-14 15:23:03 -0300

    feat: update login functionality to use UserServices and handle login response

--- src/Web/ClientApp/src/app/features/login/login.component.ts
+++ src/Web/ClientApp/src/app/features/login/login.component.ts
@@ -1,26 +1,43 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { AuthService } from '../../core/services/auth.service';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { UserServices } from '../../core/services/user.services';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
 })
 export class LoginComponent {
   email: string = '';
   password: string = '';
+  isValid: boolean = true;
 
-  constructor(private auth: AuthService, private router: Router) {}
+  constructor(private auth: UserServices, private router: Router) {}
 
   onLogin() {
     console.log('Email:', this.email, 'Password:', this.password);
 
     if (this.email && this.password) {
-      this.auth.login('fake-login');
-      this.router.navigate(['/explorer']);
+      this.auth
+        .login({ email: this.email, password: this.password })
+        .subscribe({
+          next: (response) => {
+            console.log('Login successful:', response);
+
+            if (!response.success) {
+              this.isValid = false;
+            } else {
+              this.router.navigate(['/explorer']);
+            }
+          },
+          error: (error) => {
+            console.error('Login failed:', error);
+            this.isValid = false;
+          }
+        });
     }
   }
 }
