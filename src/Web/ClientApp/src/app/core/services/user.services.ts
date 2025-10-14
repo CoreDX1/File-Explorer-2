e5475a78e2eb6d8f41b292c0a0c0456143432960
CoreDX1 <chrismaquis@gmail.com>
2025-10-14 15:22:53 -0300

    feat: implement user services with login functionality

---
+++ src/Web/ClientApp/src/app/core/services/user.services.ts
@@ -0,0 +1,53 @@
+import { HttpClient } from '@angular/common/http';
+import {  Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserServices {
+  private url: string = 'http://localhost:5252/api/user';
+
+  constructor(private http: HttpClient) {}
+
+  login(credential: LoginRequest): Observable<Response<User>> {
+    {
+     return this.http.post<Response<User>>(
+        `${this.url}/login`,
+        credential
+      );
+    }
+  }
+}
+
+interface Response<T> {
+  message: string;
+  success: boolean;
+  data: T;
+}
+
+interface User {
+  id: number;
+  username: string;
+  email: string;
+  passwordHash: string;
+  fullName: string;
+  storageQuotaBytes: number;
+  createdAt: string;
+  updatedAt: string;
+  isActive: boolean;
+  lastLoginAt: string;
+  trackingState: number;
+  modifiedProperties: any;
+}
+
+interface LoginResponse {
+  email: string;
+  fullName: string;
+  token: string;
+}
+
+interface LoginRequest {
+  email: string;
+  password: string;
+}
