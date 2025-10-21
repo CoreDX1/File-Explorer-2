a4e50ae9928529bad0c27512a4be91595fb4e94b
CoreDX1 <chrismaquis@gmail.com>
2025-10-21 16:56:34 -0300

    refactor: remove unused HTTP GET method from PermissionController

--- src/Web/Controllers/PermissionController.cs
+++ src/Web/Controllers/PermissionController.cs
@@ -57,8 +57,6 @@ public class PermissionController : ControllerBase
         // TODO: Implement grant folder permission logic
         return Ok(new { message = "Permission granted successfully" });
     }
-
-    [HttpGet]
 }
 
 public record GrantPermissionRequest(
