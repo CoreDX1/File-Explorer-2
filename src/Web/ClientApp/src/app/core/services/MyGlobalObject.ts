779d61de9be40438d7a997c631640c797600413c
CoreDX1 <chrismaquis@gmail.com>
2025-10-21 17:51:06 -0300

    Mejora el servicio

--- src/Web/ClientApp/src/app/core/services/MyGlobalObject.ts
+++ src/Web/ClientApp/src/app/core/services/MyGlobalObject.ts
@@ -1,21 +1,38 @@
-import { Injectable } from "@angular/core";
+import { isPlatformBrowser } from "@angular/common";
+import { Inject, Injectable, PLATFORM_ID } from "@angular/core";
 
 @Injectable({
   providedIn: "root",
 })
 export class MyGlobalObject {
+  private readonly USER_KEY = "user_data";
+
   private user: Data = {
     name: "",
   };
 
-  constructor() {}
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
   public setUserName(name: string): void {
     this.user.name = name;
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.setItem(this.USER_KEY, JSON.stringify(this.user));
+    }
   }
 
   public getUserName(): string {
+    if (isPlatformBrowser(this.platformId)) {
+      const user = localStorage.getItem(this.USER_KEY);
+      return user ? JSON.parse(user).name : "";
+    }
     return this.user.name;
+  }
+
+  public clearUserData(): void {
+    this.user.name = "";
+    if (isPlatformBrowser(this.platformId)) {
+      localStorage.removeItem(this.USER_KEY);
+    }
   }
 }
 
