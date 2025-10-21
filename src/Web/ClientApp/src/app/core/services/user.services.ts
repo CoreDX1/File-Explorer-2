2e7c7c05ac3eb340d90e0db5dfdf4ec1a5c22a83
CoreDX1 <chrismaquis@gmail.com>
2025-10-21 16:54:51 -0300

    style: format code for consistency and readability

--- src/Web/ClientApp/src/app/core/services/user.services.ts
+++ src/Web/ClientApp/src/app/core/services/user.services.ts
@@ -1,32 +1,29 @@
-import { HttpClient } from '@angular/common/http';
-import {  Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class UserServices {
-  private url: string = 'http://localhost:5252/api/user';
+  private url: string = "http://localhost:5252/api/user";
 
   constructor(private http: HttpClient) {}
 
   login(credential: LoginRequest): Observable<Response<User>> {
     {
-     return this.http.post<Response<User>>(
-        `${this.url}/login`,
-        credential
-      );
+      return this.http.post<Response<User>>(`${this.url}/login`, credential);
     }
   }
 }
 
-interface Response<T> {
+export interface Response<T> {
   message: string;
   success: boolean;
   data: T;
 }
 
-interface User {
+export interface User {
   id: number;
   username: string;
   email: string;
