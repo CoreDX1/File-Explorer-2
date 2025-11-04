776cdd02410c8c6387240b9c603d46e716e36335
CoreDX1 <chrismaquis@gmail.com>
2025-11-04 16:32:59 -0300

    Creando el Dto para editar al usuario

---
+++ src/Application/DTOs/Request/EditUserRequest.cs
@@ -0,0 +1,10 @@
+namespace Application.DTOs.Request;
+
+public class EditUserRequest
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Phone { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+}
