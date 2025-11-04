ae77f9e70adca6828a293a01c115671ec411a576
CoreDX1 <chrismaquis@gmail.com>
2025-11-04 16:33:28 -0300

    Comentando el metodo CreateUser y modificando el tipo de dato

--- src/Application/Interface/IUserServices.cs
+++ src/Application/Interface/IUserServices.cs
@@ -11,12 +11,14 @@ public interface IUserServices : IService<User>
 {
     public Task<User> GetUserByEmail(string email);
     public Task<List<User>> GetAllUsers();
-    public Task<ApiResult<CreateUserResponse>> CreateUser(CreateUserRequest request);
+
+    // public Task<ApiResult<CreateUserResponse>> CreateUser(CreateUserRequest request);
     public Task<ApiResult<LoginResponse>> Login(string email, string password);
 
     // New methods for AuthController
     Task<ApiResult<LoginResponse>> AuthenticateAsync(string email, string password);
-    Task<ApiResult<object>> CreateUserAsync(CreateUserRequest request);
+    Task<ApiResult<bool>> EditUser(EditUserRequest user);
+    Task<ApiResult<bool>> CreateUserAsync(CreateUserRequest request);
     Task<ApiResult<object>> RefreshTokenAsync(string refreshToken);
     Task RevokeTokenAsync(string refreshToken);
     Task<ApiResult<object>> GoogleAuthAsync(string idToken);
