a25971d38974ee9a1a8c162f300918f0a3aeb29d
CoreDX1 <chrismaquis@gmail.com>
2025-11-19 16:18:59 -0300

    refactor: add default values to EditUserRequest record parameters

--- src/Application/DTOs/Request/EditUserRequest.cs
+++ src/Application/DTOs/Request/EditUserRequest.cs
@@ -2,9 +2,9 @@ namespace Application.DTOs.Request;
 
 public sealed record EditUserRequest(
     int Id,
-    string FirstName,
-    string LastName,
-    string Phone,
-    string Password,
-    string Email
+    string FirstName = "",
+    string LastName = "",
+    string Phone = "",
+    string Password = "",
+    string Email = ""
 );
