1e51e5b2024287182b7660ab8cff795c334f5c1f
CoreDX1 <chrismaquis@gmail.com>
2025-11-19 15:18:54 -0300

    feat: convert GetUserResponseUnique from class to record

--- src/Application/DTOs/Response/GetUserResponseUnique.cs
+++ src/Application/DTOs/Response/GetUserResponseUnique.cs
@@ -1,10 +1,9 @@
 namespace Application.DTOs.Response;
 
-public class GetUserResponseUnique
-{
-    public int Id { get; set; }
-    public string FirstName { get; set; } = string.Empty;
-    public string LastName { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public string Phone { get; set; } = string.Empty;
-}
+public sealed record GetUserResponseUnique(
+    int Id,
+    string FirstName,
+    string LastName,
+    string Email,
+    string Phone
+);
