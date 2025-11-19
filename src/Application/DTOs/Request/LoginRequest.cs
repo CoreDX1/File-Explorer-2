ba8250b744008180a98f376436fdd97170f770de
CoreDX1 <chrismaquis@gmail.com>
2025-11-19 15:18:11 -0300

    feat: convert LoginRequest from class to record

--- src/Application/DTOs/Request/LoginRequest.cs
+++ src/Application/DTOs/Request/LoginRequest.cs
@@ -1,7 +1,3 @@
 namespace Application.DTOs.Request;
 
-public class LoginRequest
-{
-    public string Email { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
-}
+public sealed record LoginRequest(string Email, string Password);
