cdd74accd58692c58acf08a4b45a58ce283b4bcf
CoreDX1 <chrismaquis@gmail.com>
2025-11-20 11:50:57 -0300

    Commit DirectoryItem.cs file

--- src/Domain/Entities/DirectoryItem.cs
+++ src/Domain/Entities/DirectoryItem.cs
@@ -2,8 +2,8 @@ namespace Domain.Entities;
 
 public class DirectoryItem : FileSystemItem
 {
-    public List<FileItem> Files { get; set; } = new List<FileItem>();
-    public List<DirectoryItem> SubFolders { get; set; } = new List<DirectoryItem>();
+    public ICollection<FileItem> Files { get; } = new List<FileItem>();
+    public ICollection<DirectoryItem> SubFolders { get; } = new List<DirectoryItem>();
 
     public DirectoryItem(string name, string path, long size, DateTime createdAt, DateTime modifiedAt)
         : base(name, path, size, createdAt, modifiedAt)
