62ea8fb4ee1534a9e633fc2c7296ab147169e7dd
CoreDX1 <chrismaquis@gmail.com>
2025-11-20 17:04:00 -0300

    Commit IService.cs interface file

--- src/Application/Interfaces/IService.cs
+++ src/Application/Interfaces/IService.cs
@@ -5,7 +5,7 @@ namespace Application.Interfaces;
 public interface IService<TEntity>
     where TEntity : ITrackable
 {
-    TEntity Find(params object[] keyValues);
+    TEntity? Find(params object[] keyValues);
     IQueryable<TEntity> SelectQuery(string query, params object[] parameters);
     void Insert(TEntity entity);
     void InsertRange(IEnumerable<TEntity> entities);
@@ -13,8 +13,8 @@ public interface IService<TEntity>
     void Update(TEntity entity);
     void Delete(object id);
     void Delete(TEntity entity);
-    Task<TEntity> FindAsync(params object[] keyValues);
-    Task<TEntity> FindAsync(CancellationToken cancellationToken, params object[] keyValues);
+    Task<TEntity?> FindAsync(params object[] keyValues);
+    Task<TEntity?> FindAsync(CancellationToken cancellationToken, params object[] keyValues);
     Task<bool> DeleteAsync(params object[] keyValues);
     Task<bool> DeleteAsync(CancellationToken cancellationToken, params object[] keyValues);
     IQueryable<TEntity> Queryable();
