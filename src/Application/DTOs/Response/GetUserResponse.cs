087c55809652bd0407fe4376765959ead777e327
CoreDX1 <chrismaquis@gmail.com>
2025-11-20 17:17:54 -0300

    Commit GetUserResponse.cs DTO file

--- src/Application/DTOs/Response/GetUserResponse.cs
+++ src/Application/DTOs/Response/GetUserResponse.cs
@@ -1,15 +1,15 @@
 namespace Application.DTOs.Response;
 
-public sealed record GetUserResponse(
+public sealed record UserResponse(
     int Id,
     string FirstName,
     string LastName,
     string Email,
-    string Phone,
-    bool IsActive,
-    DateTime CreatedAt,
-    DateTime UpdatedAt,
-    DateTime LastLoginAt
+    string Phone
+// bool IsActive
+// DateTime CreatedAt,
+// DateTime UpdatedAt,
+// DateTime LastLoginAt
 )
 {
     public string FullName => $"{FirstName} {LastName}";
