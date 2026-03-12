270e19d0ede7ffb29609351110629906c0899af5
CoreDX1 <chrismaquis@gmail.com>
2026-03-12 16:48:22 -0300

    feat: hacer opcional el campo Phone en CreateUserRequest
    el campo phone ahora acepta valores nullables (string?)
    permite crear usuario sin numero telefonico

--- src/Application/DTOs/Request/CreateUserRequest.cs
+++ src/Application/DTOs/Request/CreateUserRequest.cs
@@ -3,7 +3,7 @@ namespace Application.DTOs.Request;
 public sealed record CreateUserRequest(
     string FirstName,
     string LastName,
-    string Phone,
+    string? Phone,
     string Password,
     string Email
 );
